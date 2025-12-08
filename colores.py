00c163665e7ba5e57c2ff2dddbdc763f967662d6
María Carina Roldán <maria.carina.roldan@gmail.com>
2025-12-08 08:21:54 -0300

    varios metodos de imputacion

---
+++ colores.py
@@ -0,0 +1,17 @@
+# colores para los gráficos
+colores = {
+    'estepa':   '#FF6347',  # Rojo anaranjado
+    'pino':     '#2E8B57',  # Verde bosque
+    'dia':      '#FFA726',  # Naranja suave
+    'noche':    '#4169E1',  # Azul royal
+    'datos':    '#E2725B',  # Terracota claro
+    'trend':    '#2E8B57',  # Verde bosque
+    'season':   '#4169E1',  # Azul royal
+    'residuo':  '#9E9E9E',  # Gris medio
+    'train':    '#9E9E9E',  # Gris medio
+    'test':     '#2B2B2B',  # Gris oscuro
+    'LSTM':     '#E57373',  # Rojo suave
+    'GRU':      '#6A5ACD',  # Lavanda
+    'TRANSFRMR':'#00FA9A',  # Verde menta
+    'otro':     '#C5B4E3',  # Lavanda claro
+    }
